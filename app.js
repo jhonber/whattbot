36f0e165a4e2edf69034b65842722b3dc821dc41
Jhon <jhonb3r@gmail.com>
2019-07-07 16:14:02 -0500

    Test heroku (2)

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const Telegraf = require('telegraf');
+const express = require('express');
+const expressApp = express();
 
 const API_TOKEN = process.env.BOT_TOKEN || '';
 const PORT = process.env.PORT || 3000;
@@ -6,10 +8,21 @@ const URL = process.env.URL || 'https://whattbot.herokuapp.com/';
 
 const bot = new Telegraf(API_TOKEN);
 bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
-bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
+expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
 
-bot.command('add', (ctx) => {
+bot.command('foo', (ctx) => {
   console.log('HERE')
   console.log(ctx)
-  ctx.reply('Added: ', ctx.message)
+  ctx.reply('foo ', ctx.message)
 })
+
+/*
+ your bot commands and all the other stuff on here ....
+*/
+// and at the end just start server on PORT
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+expressApp.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
