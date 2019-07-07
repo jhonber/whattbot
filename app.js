41150c6035124db9f306d296d4042465891b64d0
Jhon <jhonb3r@gmail.com>
2019-07-07 16:02:37 -0500

    Test heroku

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const API_TOKEN = process.env.BOT_TOKEN || '';
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://whattbot.herokuapp.com/';
+
+const bot = new Telegraf(API_TOKEN);
+bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
+
 bot.command('add', (ctx) => {
-  ctx.reply('Added ', ctx.message)
+  console.log('HERE')
+  console.log(ctx)
+  ctx.reply('Added: ', ctx.message)
 })
-bot.command('balance', ({ reply }) => reply('Yo'))
-bot.command('help', Telegraf.reply('λ'))
-bot.launch()
